fbc60605b8a1ed362d7d545ab2fde7e2a1ab8651
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-22 11:04:03 +0000

    Parte 5.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.backgground');
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
@@ -16,6 +21,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -28,4 +34,15 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = cactusPosition + 'px';
+
+    background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
